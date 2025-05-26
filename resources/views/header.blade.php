db9c5069a0962f0f8cbb34f516f240a2cfc572c4
carinezoe <zoecleo.dreessen@gmail.com>
2025-05-26 13:34:01 +0200

    added header

---
+++ resources/views/header.blade.php
@@ -0,0 +1,17 @@
+<head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<div class="flex flex-row justify-between pt-8 pl-6 pb-8 pr-6 text-lg font-mono rounded-lg bg-green-200 text-green-900">
+    <div>Home</div>
+    <div>Meld</div>
+    <div>Over Ons</div>
+    <div>Contact</div>
+    <div class="flex flex-row gap-4 text-green-800">
+        <div>Login</div>
+        <div>Registreer</div>
+    </div>
+    <div class="flex flex-row hidden text-green-800">
+        <div>Profiel</div>
+        <div>log uit</div>
+    </div>
+</div>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,7 @@
+<div class="bg-green-100 p-4">
+    @include('header')
+
+    <div class="mt-4">
+        @yield('content')
+    </div>
+</div>
